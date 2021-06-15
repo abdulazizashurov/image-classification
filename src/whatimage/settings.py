b7941943647f720e3c6d96b93a10cecea5c50650
Abdulaziz Ashurov <abdulazizashurov@MacBook-Pro-Abdulaziz.local>
2021-06-15 17:46:19 +0500

    Final

--- src/whatimage/settings.py
+++ src/whatimage/settings.py
@@ -24,9 +24,9 @@ MAIN_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = 'django-insecure-e!(nvj5h-s^id7f)wt9n=8qyo761oe7@g)yfg@ogwecwyf+ws!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['what1.herokuapp.com','127.0.0.1']
 
 
 # Application definition
